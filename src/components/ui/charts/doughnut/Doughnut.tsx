46d8270e0f0936ea82844edd8fa9735e0b0f2fb6
JungDahun <dahun428@naver.com>
2024-04-17 23:27:44 +0900

    make policedougnut

--- src/components/ui/charts/doughnut/Doughnut.tsx
+++ src/components/ui/charts/doughnut/Doughnut.tsx
@@ -1,69 +1,73 @@
-import { isObject } from "@/utils/object";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from "chart.js";
-import { useMemo } from "react";
-import { Chart, Doughnut as DoughnutChart } from "react-chartjs-2";
-import styles from "./Doughnut.module.scss";
-import { digit } from "@/utils/number";
+import { isObject } from '@/utils/object';
+import {
+	Chart as ChartJS,
+	ArcElement,
+	Tooltip,
+	Legend,
+	Colors,
+} from 'chart.js';
+import { useMemo } from 'react';
+import { Chart, Doughnut as DoughnutChart } from 'react-chartjs-2';
+import styles from './Doughnut.module.scss';
+import { digit } from '@/utils/number';
 
 ChartJS.register(ArcElement, Tooltip, Legend, Colors);
 
+/**
+ * 강도 : 1, 절도 : 1, 살인 : 1, 폭력 : 1,
+ */
+
 export type DoughnutDataObject = {
-  [key: string]: number;
+	[key: string]: number;
 };
 
 type Props = {
-  className?: string;
-  dataObject: DoughnutDataObject;
-  chartName?: string;
-  label: string;
-  colors?: string[];
+	className?: string;
+	dataObject: DoughnutDataObject;
+	chartName?: string;
+	label: string;
+	colors?: string[];
 };
 
-export const Doughnut: React.FC<Props> = ({ className, label, chartName, dataObject, colors }) => {
-  if (!isObject(dataObject)) return null;
-  const labels: string[] = Object.keys(dataObject);
-  const chartDatas: number[] = Object.values(dataObject);
+export const Doughnut: React.FC<Props> = ({
+	className,
+	label,
+	chartName,
+	dataObject,
+	colors,
+}) => {
+	if (!isObject(dataObject)) return null;
+	const labels: string[] = Object.keys(dataObject);
+	const chartDatas: number[] = Object.values(dataObject);
 
-  const adjustLabels = useMemo(() => {
-    const labels = Object.keys(dataObject);
-    const chartDatas: number[] = Object.values(dataObject);
+	const adjustLabels = useMemo(() => {
+		const labels = Object.keys(dataObject);
+		const chartDatas: number[] = Object.values(dataObject);
 
-    let result = [];
-    for (let i = 0; i < labels.length; i++) {
-      const text = `${labels[i]} (${digit(chartDatas[i])})`;
-      result.push(text);
-    }
-    return result;
-  }, [dataObject]);
+		let result = [];
+		for (let i = 0; i < labels.length; i++) {
+			const text = `${labels[i]} (${digit(chartDatas[i])})`;
+			result.push(text);
+		}
+		return result;
+	}, [dataObject]);
 
-  if (labels.length > 6) {
-    console.log("Do not over 6 elements in chart, now : ", labels.length);
-    return null;
-  }
-
-  const defaultColor = useMemo(() => {
-    if (colors && colors.length === labels.length) {
-      return colors;
-    }
-    return ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"].slice(0, labels.length);
-  }, [colors, labels]);
-
-  const data = {
-    labels: adjustLabels,
-    datasets: [
-      {
-        label: label,
-        data: chartDatas,
-      },
-    ],
-  };
-  const options = {};
-  return (
-    <div className={className}>
-      {chartName && <div className={styles.chartName}>{chartName}</div>}
-      <DoughnutChart data={data} options={options} />
-    </div>
-  );
+	const data = {
+		labels: adjustLabels,
+		datasets: [
+			{
+				label: label,
+				data: chartDatas,
+			},
+		],
+	};
+	const options = {};
+	return (
+		<div className={className}>
+			{chartName && <div className={styles.chartName}>{chartName}</div>}
+			<DoughnutChart data={data} options={options} />
+		</div>
+	);
 };
 
-Doughnut.displayName = "Doughnut";
+Doughnut.displayName = 'Doughnut';
--- src/utils/openapi/police/police.ts
+++ src/utils/openapi/police/police.ts
@@ -1,59 +1,88 @@
-import { Police } from "@/models/api/open/police/SearchPoliceResponse";
-import { Violence } from "@/store/modules/common/violence";
+import { Police } from '@/models/api/open/police/SearchPoliceResponse';
+import { Violence } from '@/store/modules/common/violence';
 
-export const policeCityArray = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주", "세종"];
+export const policeCityArray = [
+	'서울',
+	'부산',
+	'대구',
+	'인천',
+	'광주',
+	'대전',
+	'울산',
+	'경기',
+	'강원',
+	'충북',
+	'충남',
+	'전북',
+	'전남',
+	'경북',
+	'경남',
+	'제주',
+	'세종',
+];
+
+export const PoliceType = {
+	ROBBER: '강도',
+	MURDER: '살인',
+	THEFT: '절도',
+	VIOLENCE: '폭력',
+} as const;
+
+export type PoliceType = (typeof PoliceType)[keyof typeof PoliceType];
 
 export const seperateByCity = (data: Police[], targetName?: string) => {
-  const cityNames = new Set<string>();
-  data.forEach((item, index) => {
-    const name = item.경찰서.slice(0, 2);
-    cityNames.add(name);
-  });
-  return Array.from(cityNames).map((item, index) => {
-    return data.filter((newItem, newIndex) => {
-      if (newItem.경찰서.includes(item)) {
-        return newItem;
-      }
-    });
-  });
+	const cityNames = new Set<string>();
+	data.forEach((item, index) => {
+		const name = item.경찰서.slice(0, 2);
+		cityNames.add(name);
+	});
+	return Array.from(cityNames).map((item, index) => {
+		return data.filter((newItem, newIndex) => {
+			if (newItem.경찰서.includes(item)) {
+				return newItem;
+			}
+		});
+	});
 };
 
 export const mergeByCity = (data: Police[]) => {
-  const cityNames = new Set<string>();
-  policeCityArray.map((item) => {
-    cityNames.add(item);
-  });
-  return Array.from(cityNames).map((item, index) => {
-    let 강도 = 0;
-    let 살인 = 0;
-    let 절도 = 0;
-    let 폭력 = 0;
-    data.filter((newItem, newIndex) => {
-      if (newItem.경찰서.includes(item)) {
-        강도 += newItem.강도;
-        살인 += newItem.살인;
-        절도 += newItem.절도;
-        폭력 += newItem.폭력;
-      }
-    });
-    return {
-      city: item,
-      강도,
-      살인,
-      절도,
-      폭력,
-    };
-  });
+	const cityNames = new Set<string>();
+	policeCityArray.map(item => {
+		cityNames.add(item);
+	});
+	return Array.from(cityNames).map((item, index) => {
+		let 강도 = 0;
+		let 살인 = 0;
+		let 절도 = 0;
+		let 폭력 = 0;
+		data.filter((newItem, newIndex) => {
+			if (newItem.경찰서.includes(item)) {
+				강도 += newItem.강도;
+				살인 += newItem.살인;
+				절도 += newItem.절도;
+				폭력 += newItem.폭력;
+			}
+		});
+		return {
+			city: item,
+			강도,
+			살인,
+			절도,
+			폭력,
+		};
+	});
 };
 
 export const mergeByYearly = (violences: Violence[]) => {
-  return violences.map((violence, index) => {
-    const _data = mergeByCity(violence.data);
-    const year = violence.data[0].발생년도 ? `${violence.data[0].발생년도}` : `${violence.data[0].발생연도}`;
-    // return { [year]: _data };
-    return { [year]: _data };
-  });
-  // return result;
+	return violences.map((violence, index) => {
+		const _data = mergeByCity(violence.data);
+		const year = violence.data[0].발생년도
+			? `${violence.data[0].발생년도}`
+			: `${violence.data[0].발생연도}`;
+		// return { [year]: _data };
+		return { [year]: _data };
+	});
+	// return result;
 };
 
 /**
@@ -64,43 +93,45 @@ export const mergeByYearly = (violences: Violence[]) => {
  *   폭력 : [2014,2015,2016,2017,2018,2019,2020,2021,2022]
  */
 export const mergeByCityWithYear = (violences: Violence[]) => {
-  const mergedByYearly = mergeByYearly(violences);
+	const mergedByYearly = mergeByYearly(violences);
 
-  let array: mergeByCityWithYearType[] = [];
+	let array: mergeByCityWithYearType[] = [];
 
-  mergedByYearly.forEach((item, index) => {
-    const items = Object.values(item);
-    items.map((level2Item) => {
-      level2Item.map((level3Item) => {
-        const { city, 강도, 살인, 절도, 폭력 } = level3Item;
-        const foundIndex = array.findIndex((arrayItem) => arrayItem.city === city);
-        if (foundIndex >= 0) {
-          array[foundIndex].강도 = [...array[foundIndex].강도, 강도];
-          array[foundIndex].살인 = [...array[foundIndex].살인, 살인];
-          array[foundIndex].절도 = [...array[foundIndex].절도, 절도];
-          array[foundIndex].폭력 = [...array[foundIndex].폭력, 폭력];
-        } else {
-          let newArray = {
-            city,
-            강도: [강도],
-            살인: [살인],
-            절도: [절도],
-            폭력: [폭력],
-          };
+	mergedByYearly.forEach((item, index) => {
+		const items = Object.values(item);
+		items.map(level2Item => {
+			level2Item.map(level3Item => {
+				const { city, 강도, 살인, 절도, 폭력 } = level3Item;
+				const foundIndex = array.findIndex(
+					arrayItem => arrayItem.city === city
+				);
+				if (foundIndex >= 0) {
+					array[foundIndex].강도 = [...array[foundIndex].강도, 강도];
+					array[foundIndex].살인 = [...array[foundIndex].살인, 살인];
+					array[foundIndex].절도 = [...array[foundIndex].절도, 절도];
+					array[foundIndex].폭력 = [...array[foundIndex].폭력, 폭력];
+				} else {
+					let newArray = {
+						city,
+						강도: [강도],
+						살인: [살인],
+						절도: [절도],
+						폭력: [폭력],
+					};
 
-          array.push(newArray);
-        }
-      });
-    });
-  });
+					array.push(newArray);
+				}
+			});
+		});
+	});
 
-  return Array.from(array);
+	return Array.from(array);
 };
 
 type mergeByCityWithYearType = {
-  city: string;
-  강도: number[];
-  살인: number[];
-  절도: number[];
-  폭력: number[];
+	city: string;
+	강도: number[];
+	살인: number[];
+	절도: number[];
+	폭력: number[];
 };
