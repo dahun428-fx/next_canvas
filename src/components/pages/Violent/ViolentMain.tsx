d228ba40e7ceff42763c82a49247be6f7d8535c5
JungDahun <dahun428@naver.com>
2024-04-20 12:51:30 +0900

    add all

--- src/components/pages/Violent/ViolentMain.tsx
+++ src/components/pages/Violent/ViolentMain.tsx
@@ -1,134 +1,218 @@
-import { VerticalBar } from "@/components/ui/charts/verticalbar";
-import { TableBasic } from "@/components/ui/tables/tableBasic";
-import { ViolenceState } from "@/store/modules/common/violence";
-import { PoliceType, mergeByCityWithYear, mergeByYearly, policeCityArray } from "@/utils/openapi/police/police";
-import { MouseEvent, SyntheticEvent, useEffect, useMemo, useRef, useState } from "react";
-import { ViolentTable } from "./ViolentTable/ViolentTable";
-import { SwitchBasic } from "@/components/ui/list/switch";
-import styles from "./ViolentMain.module.scss";
-import { Box, Grid, Tab } from "@mui/material";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { ViolentParts } from "./ViolentParts/ViolentParts";
-import { ChartType, ChartTypeRegistry } from "chart.js";
+import { VerticalBar } from '@/components/ui/charts/verticalbar';
+import { TableBasic } from '@/components/ui/tables/tableBasic';
+import { ViolenceState } from '@/store/modules/common/violence';
+import {
+	PoliceType,
+	mergeByCityWithYear,
+	mergeByYearly,
+	policeCityArray,
+} from '@/utils/openapi/police/police';
+import {
+	MouseEvent,
+	SyntheticEvent,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from 'react';
+import { ViolentTable } from './ViolentTable/ViolentTable';
+import { SwitchBasic } from '@/components/ui/list/switch';
+import styles from './ViolentMain.module.scss';
+import { Box, Grid, Tab } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { ViolentParts } from './ViolentParts/ViolentParts';
+import { ChartType, ChartTypeRegistry } from 'chart.js';
 
 // export type ViolentAcceptableChartType = Pick<ChartTypeRegistry, "line" | "bar" | "polarArea">;
 
 export interface ViolentData {
-  label: string;
-  data: number[];
+	label: string;
+	data: number[];
 }
 
 type Props = {
-  violenceResponse: ViolenceState;
-  selectedChart: ChartType;
+	violenceResponse: ViolenceState;
+	selectedChart: ChartType;
 };
 
 const cityNames = policeCityArray;
 
-export const ViolentMain: React.FC<Props> = ({ violenceResponse, selectedChart }) => {
-  if (violenceResponse.items.length < 1) return null;
+export const ViolentMain: React.FC<Props> = ({
+	violenceResponse,
+	selectedChart,
+}) => {
+	if (violenceResponse.items.length < 1) return null;
 
-  const [tabValue, setTabValue] = useState<PoliceType>(PoliceType.ROBBER);
+	const [tabValue, setTabValue] = useState<PoliceType>(PoliceType.ROBBER);
 
-  const [checkedCityNamesWithRobber, setCheckedCityNamesWithRobber] = useState<string[]>(cityNames);
-  const [checkedCityNamesWithMurder, setCheckedCityNamesWithMurder] = useState<string[]>(cityNames);
-  const [checkedCityNamesWithTheft, setCheckedCityNamesWithTheft] = useState<string[]>(cityNames);
-  const [checkedCityNamesWithViolence, setCheckedCityNamesWithViolence] = useState<string[]>(cityNames);
+	const [checkedCityNamesWithRobber, setCheckedCityNamesWithRobber] =
+		useState<string[]>(cityNames);
+	const [checkedCityNamesWithMurder, setCheckedCityNamesWithMurder] =
+		useState<string[]>(cityNames);
+	const [checkedCityNamesWithTheft, setCheckedCityNamesWithTheft] =
+		useState<string[]>(cityNames);
+	const [checkedCityNamesWithViolence, setCheckedCityNamesWithViolence] =
+		useState<string[]>(cityNames);
+	const [checkedCityNamesWithTotal, setCheckedCityNamesWithTotal] =
+		useState<string[]>(cityNames);
 
-  const changeCheckedCityNamesWithRobber = (items: string[]) => {
-    setCheckedCityNamesWithRobber(Array.from(new Set(items)));
-  };
-  const changeCheckedCityNamesWithMurder = (items: string[]) => {
-    setCheckedCityNamesWithMurder(Array.from(new Set(items)));
-  };
-  const changeCheckedCityNamesWithTheft = (items: string[]) => {
-    setCheckedCityNamesWithTheft(Array.from(new Set(items)));
-  };
-  const changeCheckedCityNamesWithViolence = (items: string[]) => {
-    setCheckedCityNamesWithViolence(Array.from(new Set(items)));
-  };
+	const changeCheckedCityNamesWithRobber = (items: string[]) => {
+		setCheckedCityNamesWithRobber(Array.from(new Set(items)));
+	};
+	const changeCheckedCityNamesWithMurder = (items: string[]) => {
+		setCheckedCityNamesWithMurder(Array.from(new Set(items)));
+	};
+	const changeCheckedCityNamesWithTheft = (items: string[]) => {
+		setCheckedCityNamesWithTheft(Array.from(new Set(items)));
+	};
+	const changeCheckedCityNamesWithViolence = (items: string[]) => {
+		setCheckedCityNamesWithViolence(Array.from(new Set(items)));
+	};
+	const changeCheckedCityNamesWithTotal = (items: string[]) => {
+		setCheckedCityNamesWithTotal(Array.from(new Set(items)));
+	};
 
-  const labels = useMemo(() => {
-    const mergedData = mergeByYearly(violenceResponse.items);
-    return mergedData.map((item, index) => {
-      return Object.keys(item)[0];
-    });
-  }, [violenceResponse]);
+	const labels = useMemo(() => {
+		const mergedData = mergeByYearly(violenceResponse.items);
+		return mergedData.map((item, index) => {
+			return Object.keys(item)[0];
+		});
+	}, [violenceResponse]);
 
-  const datasForRobber = useMemo(() => {
-    let result: ViolentData[] = [];
-    mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
-      if (checkedCityNamesWithRobber.includes(item.city)) {
-        result.push({ label: item.city, data: item.강도 });
-      }
-    });
+	const datasTotal = useMemo(() => {
+		let result: ViolentData[] = [];
+		mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
+			if (checkedCityNamesWithTotal.includes(item.city)) {
+				result.push({ label: item.city, data: item.total });
+			}
+		});
 
-    return result;
-  }, [violenceResponse, checkedCityNamesWithRobber]);
-  const datasForMurder = useMemo(() => {
-    let result: ViolentData[] = [];
-    mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
-      if (checkedCityNamesWithMurder.includes(item.city)) {
-        result.push({ label: item.city, data: item.살인 });
-      }
-    });
+		return result;
+	}, [violenceResponse, checkedCityNamesWithTotal]);
 
-    return result;
-  }, [violenceResponse, checkedCityNamesWithMurder]);
-  const datasForTheft = useMemo(() => {
-    let result: ViolentData[] = [];
-    mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
-      if (checkedCityNamesWithTheft.includes(item.city)) {
-        result.push({ label: item.city, data: item.절도 });
-      }
-    });
+	const datasForRobber = useMemo(() => {
+		let result: ViolentData[] = [];
+		mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
+			if (checkedCityNamesWithRobber.includes(item.city)) {
+				result.push({ label: item.city, data: item.강도 });
+			}
+		});
 
-    return result;
-  }, [violenceResponse, checkedCityNamesWithTheft]);
-  const datasForViolence = useMemo(() => {
-    let result: ViolentData[] = [];
-    mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
-      if (checkedCityNamesWithViolence.includes(item.city)) {
-        result.push({ label: item.city, data: item.폭력 });
-      }
-    });
+		return result;
+	}, [violenceResponse, checkedCityNamesWithRobber]);
+	const datasForMurder = useMemo(() => {
+		let result: ViolentData[] = [];
+		mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
+			if (checkedCityNamesWithMurder.includes(item.city)) {
+				result.push({ label: item.city, data: item.살인 });
+			}
+		});
 
-    return result;
-  }, [violenceResponse, checkedCityNamesWithViolence]);
+		return result;
+	}, [violenceResponse, checkedCityNamesWithMurder]);
+	const datasForTheft = useMemo(() => {
+		let result: ViolentData[] = [];
+		mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
+			if (checkedCityNamesWithTheft.includes(item.city)) {
+				result.push({ label: item.city, data: item.절도 });
+			}
+		});
 
-  return (
-    <div>
-      <TabContext value={tabValue}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList
-            onChange={(event: SyntheticEvent, value: PoliceType) => {
-              setTabValue(value);
-            }}
-            // variant="scrollable"
-            centered
-            // aria-label="lab API tabs example"
-          >
-            <Tab label={`${PoliceType.ROBBER}`} value={PoliceType.ROBBER} />
-            <Tab label={`${PoliceType.MURDER}`} value={PoliceType.MURDER} />
-            <Tab label={`${PoliceType.THEFT}`} value={PoliceType.THEFT} />
-            <Tab label={`${PoliceType.VIOLENCE}`} value={PoliceType.VIOLENCE} />
-          </TabList>
-        </Box>
-        <TabPanel value={PoliceType.ROBBER}>
-          <ViolentParts selectedChart={selectedChart} labels={labels} cityNames={cityNames} title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.ROBBER})`} datas={datasForRobber} violenceResponse={violenceResponse} changeCheckedCityNames={changeCheckedCityNamesWithRobber} checkedCityName={checkedCityNamesWithRobber} />
-        </TabPanel>
-        <TabPanel value={PoliceType.MURDER}>
-          <ViolentParts selectedChart={selectedChart} labels={labels} cityNames={cityNames} title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.MURDER})`} datas={datasForMurder} violenceResponse={violenceResponse} changeCheckedCityNames={changeCheckedCityNamesWithMurder} checkedCityName={checkedCityNamesWithMurder} />
-        </TabPanel>
-        <TabPanel value={PoliceType.THEFT}>
-          <ViolentParts selectedChart={selectedChart} labels={labels} cityNames={cityNames} title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.THEFT})`} datas={datasForTheft} violenceResponse={violenceResponse} changeCheckedCityNames={changeCheckedCityNamesWithTheft} checkedCityName={checkedCityNamesWithTheft} />
-        </TabPanel>
-        <TabPanel value={PoliceType.VIOLENCE}>
-          <ViolentParts selectedChart={selectedChart} labels={labels} cityNames={cityNames} title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.VIOLENCE})`} datas={datasForViolence} violenceResponse={violenceResponse} changeCheckedCityNames={changeCheckedCityNamesWithViolence} checkedCityName={checkedCityNamesWithViolence} />
-        </TabPanel>
-      </TabContext>
-    </div>
-  );
+		return result;
+	}, [violenceResponse, checkedCityNamesWithTheft]);
+	const datasForViolence = useMemo(() => {
+		let result: ViolentData[] = [];
+		mergeByCityWithYear(violenceResponse.items).forEach((item, index) => {
+			if (checkedCityNamesWithViolence.includes(item.city)) {
+				result.push({ label: item.city, data: item.폭력 });
+			}
+		});
+
+		return result;
+	}, [violenceResponse, checkedCityNamesWithViolence]);
+
+	return (
+		<div>
+			<TabContext value={tabValue}>
+				<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+					<TabList
+						onChange={(event: SyntheticEvent, value: PoliceType) => {
+							setTabValue(value);
+						}}
+						// variant="scrollable"
+						centered
+						// aria-label="lab API tabs example"
+					>
+						<Tab label={`전체`} value={'all'} />
+						<Tab label={`${PoliceType.ROBBER}`} value={PoliceType.ROBBER} />
+						<Tab label={`${PoliceType.MURDER}`} value={PoliceType.MURDER} />
+						<Tab label={`${PoliceType.THEFT}`} value={PoliceType.THEFT} />
+						<Tab label={`${PoliceType.VIOLENCE}`} value={PoliceType.VIOLENCE} />
+					</TabList>
+				</Box>
+				<TabPanel value={'all'}>
+					<ViolentParts
+						selectedChart={selectedChart}
+						labels={labels}
+						cityNames={cityNames}
+						title={`연도별 / 지역별 강력범죄 추이 ( 전체 )`}
+						datas={datasTotal}
+						violenceResponse={violenceResponse}
+						changeCheckedCityNames={changeCheckedCityNamesWithTotal}
+						checkedCityName={checkedCityNamesWithTotal}
+					/>
+				</TabPanel>
+				<TabPanel value={PoliceType.ROBBER}>
+					<ViolentParts
+						selectedChart={selectedChart}
+						labels={labels}
+						cityNames={cityNames}
+						title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.ROBBER})`}
+						datas={datasForRobber}
+						violenceResponse={violenceResponse}
+						changeCheckedCityNames={changeCheckedCityNamesWithRobber}
+						checkedCityName={checkedCityNamesWithRobber}
+					/>
+				</TabPanel>
+				<TabPanel value={PoliceType.MURDER}>
+					<ViolentParts
+						selectedChart={selectedChart}
+						labels={labels}
+						cityNames={cityNames}
+						title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.MURDER})`}
+						datas={datasForMurder}
+						violenceResponse={violenceResponse}
+						changeCheckedCityNames={changeCheckedCityNamesWithMurder}
+						checkedCityName={checkedCityNamesWithMurder}
+					/>
+				</TabPanel>
+				<TabPanel value={PoliceType.THEFT}>
+					<ViolentParts
+						selectedChart={selectedChart}
+						labels={labels}
+						cityNames={cityNames}
+						title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.THEFT})`}
+						datas={datasForTheft}
+						violenceResponse={violenceResponse}
+						changeCheckedCityNames={changeCheckedCityNamesWithTheft}
+						checkedCityName={checkedCityNamesWithTheft}
+					/>
+				</TabPanel>
+				<TabPanel value={PoliceType.VIOLENCE}>
+					<ViolentParts
+						selectedChart={selectedChart}
+						labels={labels}
+						cityNames={cityNames}
+						title={`연도별 / 지역별 강력범죄 추이 (${PoliceType.VIOLENCE})`}
+						datas={datasForViolence}
+						violenceResponse={violenceResponse}
+						changeCheckedCityNames={changeCheckedCityNamesWithViolence}
+						checkedCityName={checkedCityNamesWithViolence}
+					/>
+				</TabPanel>
+			</TabContext>
+		</div>
+	);
 };
 
-ViolentMain.displayName = "ViolentMain";
+ViolentMain.displayName = 'ViolentMain';
--- src/utils/openapi/police/police.ts
+++ src/utils/openapi/police/police.ts
@@ -1,83 +1,103 @@
-import { Police } from "@/models/api/open/police/SearchPoliceResponse";
-import { Violence } from "@/store/modules/common/violence";
+import { Police } from '@/models/api/open/police/SearchPoliceResponse';
+import { Violence } from '@/store/modules/common/violence';
 
-export const policeCityArray = ["서울", "부산", "대구", "인천", "광주", "대전", "울산", "경기", "강원", "충북", "충남", "전북", "전남", "경북", "경남", "제주", "세종"];
+export const policeCityArray = [
+	'서울',
+	'부산',
+	'대구',
+	'인천',
+	'광주',
+	'대전',
+	'울산',
+	'경기',
+	'강원',
+	'충북',
+	'충남',
+	'전북',
+	'전남',
+	'경북',
+	'경남',
+	'제주',
+	'세종',
+];
 
 export const policeChartColor: { [key: string]: string } = {
-  서울: "#FFC0CB",
-  부산: "#FFDAB9",
-  대구: "#E6E6FA",
-  인천: "#B0E0E6",
-  광주: "#F0E68C",
-  대전: "#F5FFFA",
-  울산: "#FFFACD",
-  경기: "#77DD77",
-  강원: "#89CFF0",
-  충북: "#FDFD96",
-  충남: "#F4C2C2",
-  전북: "#CA9BF7",
-  전남: "#A0D8EF",
-  경북: "#F498AD",
-  경남: "#87CEEB",
-  제주: "#A7DBA8",
-  세종: "#E6A4B4",
+	서울: '#FFC0CB',
+	부산: '#FFDAB9',
+	대구: '#E6E6FA',
+	인천: '#B0E0E6',
+	광주: '#F0E68C',
+	대전: '#F5FFFA',
+	울산: '#FFFACD',
+	경기: '#77DD77',
+	강원: '#89CFF0',
+	충북: '#FDFD96',
+	충남: '#F4C2C2',
+	전북: '#CA9BF7',
+	전남: '#A0D8EF',
+	경북: '#F498AD',
+	경남: '#87CEEB',
+	제주: '#A7DBA8',
+	세종: '#E6A4B4',
 };
 
 export const PoliceType = {
-  ROBBER: "강도",
-  MURDER: "살인",
-  THEFT: "절도",
-  VIOLENCE: "폭력",
+	ROBBER: '강도',
+	MURDER: '살인',
+	THEFT: '절도',
+	VIOLENCE: '폭력',
 } as const;
 
 export type PoliceType = (typeof PoliceType)[keyof typeof PoliceType];
 
 export const seperateByCity = (data: Police[], targetName?: string) => {
-  const cityNames = new Set<string>(policeCityArray);
+	const cityNames = new Set<string>(policeCityArray);
 
-  return Array.from(cityNames).map((item, index) => {
-    return data.filter((newItem, newIndex) => {
-      if (newItem.경찰서.slice(0, 2) === item) {
-        return newItem;
-      }
-    });
-  });
+	return Array.from(cityNames).map((item, index) => {
+		return data.filter((newItem, newIndex) => {
+			if (newItem.경찰서.slice(0, 2) === item) {
+				return newItem;
+			}
+		});
+	});
 };
 
 export const mergeByCity = (data: Police[]) => {
-  const cityNames = new Set<string>(policeCityArray);
+	const cityNames = new Set<string>(policeCityArray);
 
-  return Array.from(cityNames).map((item, index) => {
-    let 강도 = 0;
-    let 살인 = 0;
-    let 절도 = 0;
-    let 폭력 = 0;
-    data.filter((newItem, newIndex) => {
-      if (newItem.경찰서.slice(0, 2) === item) {
-        강도 += newItem.강도;
-        살인 += newItem.살인;
-        절도 += newItem.절도;
-        폭력 += newItem.폭력;
-      }
-    });
-    return {
-      city: item,
-      강도,
-      살인,
-      절도,
-      폭력,
-    };
-  });
+	return Array.from(cityNames).map((item, index) => {
+		let 강도 = 0;
+		let 살인 = 0;
+		let 절도 = 0;
+		let 폭력 = 0;
+		data.filter((newItem, newIndex) => {
+			if (newItem.경찰서.slice(0, 2) === item) {
+				강도 += newItem.강도;
+				살인 += newItem.살인;
+				절도 += newItem.절도;
+				폭력 += newItem.폭력;
+			}
+		});
+		return {
+			city: item,
+			강도,
+			살인,
+			절도,
+			폭력,
+		};
+	});
 };
 
 export const mergeByYearly = (violences: Violence[]) => {
-  return violences.map((violence, index) => {
-    const _data = mergeByCity(violence.data);
-    const year = violence.data[0].발생년도 ? `${violence.data[0].발생년도}` : `${violence.data[0].발생연도}`;
-    // return { [year]: _data };
-    return { [year]: _data };
-  });
-  // return result;
+	return violences.map((violence, index) => {
+		const _data = mergeByCity(violence.data);
+		const year = violence.data[0].발생년도
+			? `${violence.data[0].발생년도}`
+			: `${violence.data[0].발생연도}`;
+		// return { [year]: _data };
+		return { [year]: _data };
+	});
+	// return result;
 };
 
 /**
@@ -88,43 +108,49 @@ export const mergeByYearly = (violences: Violence[]) => {
  *   폭력 : [2014,2015,2016,2017,2018,2019,2020,2021,2022]
  */
 export const mergeByCityWithYear = (violences: Violence[]) => {
-  const mergedByYearly = mergeByYearly(violences);
+	const mergedByYearly = mergeByYearly(violences);
 
-  let array: mergeByCityWithYearType[] = [];
+	let array: mergeByCityWithYearType[] = [];
 
-  mergedByYearly.forEach((item, index) => {
-    const items = Object.values(item);
-    items.map((level2Item) => {
-      level2Item.map((level3Item) => {
-        const { city, 강도, 살인, 절도, 폭력 } = level3Item;
-        const foundIndex = array.findIndex((arrayItem) => arrayItem.city === city);
-        if (foundIndex >= 0) {
-          array[foundIndex].강도 = [...array[foundIndex].강도, 강도];
-          array[foundIndex].살인 = [...array[foundIndex].살인, 살인];
-          array[foundIndex].절도 = [...array[foundIndex].절도, 절도];
-          array[foundIndex].폭력 = [...array[foundIndex].폭력, 폭력];
-        } else {
-          let newArray = {
-            city,
-            강도: [강도],
-            살인: [살인],
-            절도: [절도],
-            폭력: [폭력],
-          };
+	mergedByYearly.forEach((item, index) => {
+		const items = Object.values(item);
+		items.map(level2Item => {
+			level2Item.map(level3Item => {
+				const { city, 강도, 살인, 절도, 폭력 } = level3Item;
+				const foundIndex = array.findIndex(
+					arrayItem => arrayItem.city === city
+				);
+				const total = 강도 + 살인 + 절도 + 폭력;
+				if (foundIndex >= 0) {
+					array[foundIndex].강도 = [...array[foundIndex].강도, 강도];
+					array[foundIndex].살인 = [...array[foundIndex].살인, 살인];
+					array[foundIndex].절도 = [...array[foundIndex].절도, 절도];
+					array[foundIndex].폭력 = [...array[foundIndex].폭력, 폭력];
+					array[foundIndex].total = [...array[foundIndex].total, total];
+				} else {
+					let newArray = {
+						city,
+						강도: [강도],
+						살인: [살인],
+						절도: [절도],
+						폭력: [폭력],
+						total: [total],
+					};
 
-          array.push(newArray);
-        }
-      });
-    });
-  });
+					array.push(newArray);
+				}
+			});
+		});
+	});
 
-  return Array.from(array);
+	return Array.from(array);
 };
 
 type mergeByCityWithYearType = {
-  city: string;
-  강도: number[];
-  살인: number[];
-  절도: number[];
-  폭력: number[];
+	city: string;
+	강도: number[];
+	살인: number[];
+	절도: number[];
+	폭력: number[];
+	total: number[];
 };
