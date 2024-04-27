d334be725d574be19836f375c8aa2908a6065a2f
JungDahun <dahun428@naver.com>
2024-04-27 22:55:40 +0900

    home chngae

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,11 @@ import { Inter } from 'next/font/google';
 export default function Home() {
 	return (
 		<>
-			<div>This is Home</div>
+			<div>안녕하세요.</div>
+			<div>경찰청 공공데이터API 를 활용한</div>
+			<div>대시보드 입니다. </div>
+			<div>화면 왼쪽의 네비게이션을 클릭하여</div>
+			<div>이용해보세요</div>
 		</>
 	);
 }
