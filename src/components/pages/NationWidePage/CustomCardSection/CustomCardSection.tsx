1a5c952732c0c8025ceb06bd086b48eacae23f13
JungDahun <dahun428@naver.com>
2024-04-30 16:13:20 +0900

    nation wide dev commit

---
+++ src/components/pages/NationWidePage/CustomCardSection/CustomCardSection.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type Props = {};
+export const CustomCardSection: React.FC<Props> = ({}) => {
+	return <div></div>;
+};
+
+CustomCardSection.displayName = 'CustomCardSection';
